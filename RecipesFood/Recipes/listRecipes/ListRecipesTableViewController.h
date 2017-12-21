ea45d11242c729eade75fcf523843e59f9638b1c
Cássio Sousa <cassio.sousa@accurate.com.br>
2017-12-20 22:59:47 -0200

    new windows, call custom api mcs

---
+++ RecipesFood/Recipes/listRecipes/ListRecipesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListRecipesTableViewController.h
+//  RecipesFood
+//
+//  Created by Cássio Sousa on 20/12/2017.
+//  Copyright © 2017 Cassio Sousa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListRecipesTableViewController : UITableViewController
+
+@end
